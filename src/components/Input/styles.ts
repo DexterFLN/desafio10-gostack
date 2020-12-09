0d78bc99af3b0db1a42a98f40f51e4b0e9d3f3b1
DexterFLN <dragon_dexter-x@yahoo.com.br>
2020-12-09 18:09:13 -0300

    Challenge code

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -8,13 +8,11 @@ interface IContainerProps {
 export const Container = styled.div<IContainerProps>`
   display: flex;
   align-items: center;
-
   background: #fff;
   border-radius: 8px;
   padding: 18px 24px;
   width: 100%;
   font-size: 16px;
-
   & + div {
     margin-top: 24px;
   }
@@ -25,31 +23,26 @@ export const Container = styled.div<IContainerProps>`
     font-size: 36px;
     line-height: 36px;
   }
-
   ${props =>
     props.isFocused &&
     css`
       color: #ff9000;
       border-color: #ff9000;
     `}
-
   ${props =>
     props.isFilled &&
     css`
       color: #ff9000;
     `}
-
   input {
     flex: 1;
     background: transparent;
     border: 0;
     color: #b7b7cc;
-
     &::placeholder {
       color: #b7b7cc;
     }
   }
-
   svg {
     margin-right: 16px;
   }
